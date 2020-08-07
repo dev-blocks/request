6cf83b28331987efb814e05e0bb610366139cdeb
retrofox <rdsuarez@gmail.com>
2020-08-07 10:48:54 -0300

    request and populate endpoints by namespace

---
+++ src/constants.js
@@ -0,0 +1,22 @@
+export const METHODS = {
+	get: {
+		label: 'GET',
+		enabled: true,
+	},
+	post: {
+		label: 'POST',
+		enabled: true,
+	},
+	put: {
+		label: 'PUT',
+		enabled: true,
+	},
+	patch: {
+		label: 'PATCH',
+		enabled: true,
+	},
+	delete: {
+		label: 'DELETE',
+		enabled: true,
+	}
+};
--- src/edit.js
+++ src/edit.js
@@ -1,3 +1,9 @@
+
+/**
+ * External dependencies
+ */
+import { map, values, filter } from 'lodash';
+
 /**
  * WordPress dependencies
  */
@@ -10,8 +16,8 @@ import { PanelBody, SelectControl } from '@wordpress/components';
  * Internal dependencies
  */
 import { endpointsAutocompleter } from './components/autocomplete';
-import { requestNamespaces } from './utils/request-api';
-
+import { requestNamespaces, requestEndpoints } from './utils/request-api';
+import { METHODS } from './constants';
 
 import './editor.scss';
 
@@ -20,6 +26,7 @@ export default function RequestEdit( { className, attributes, setAttributes } ) 
 	const [ placeholder, setPlaceholder ] = useState( __( 'Type / to show available endpoints', 'dev-blocks' ) );
 	const [ endpoints, setEndpoints ] = useState( [] );
 	const [ namespaces, storeNamespaces ] = useState( [] );
+	const [ filterMethods, setFilterMethods ] = useState( METHODS );
 
 	// Attributes - values.
 	const { value, namespace, endpointId } = attributes;
@@ -65,7 +72,20 @@ export default function RequestEdit( { className, attributes, setAttributes } ) 
 		} else {
 			setPlaceholder( `[${ namespace }]. Picking up endpoints ...` );
 		}
-	}, [ namespace ] );
+	}, [ namespace, endpoints ] );
+
+	/*
+	 * Effect hook - Request and populate endpoints, filtering by methods.
+	 */
+	useEffect( () => {
+		const filterMethodsArray = map( values( filter( filterMethods, m => ! m.enabled ) ), 'label' );
+		requestEndpoints({
+			path: namespace,
+			filterMethods: filterMethodsArray,
+		} )
+			.then( setEndpoints )
+			.catch( console.error );
+	}, [ namespace, filterMethods ] );
 
 	return (
 		<Fragment>
