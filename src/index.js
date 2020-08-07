b44dca00a26a614e45746040d5744895c7ee4c92
retrofox <rdsuarez@gmail.com>
2020-08-07 11:09:38 -0300

    forgot to add attrs \m/

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ import './style.scss';
  */
 import Edit from './edit';
 import save from './save';
+import attributes from './attributes';
 
 registerBlockType( 'dev-blocks/request', {
 	title: __( 'Request', 'dev-blocks' ),
@@ -20,6 +21,8 @@ registerBlockType( 'dev-blocks/request', {
 		'dev-blocks'
 	),
 	category: 'dev-blocks',
+
+	attributes,
 
 	icon: 'arrow-right-alt2',
 
